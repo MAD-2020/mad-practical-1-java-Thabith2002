31eba3a761e6e8a7c6b8d6e1084034bb0dc7645e
Thabith2002 <64420556+Thabith2002@users.noreply.github.com>
2020-04-27 23:27:18 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,17 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter integer: ");
+    int num = in.nextInt();
     
-  }
+    // Closing Scanner after the use
+    in.close();
+
+    // Calculating product of two numbers
+    int product = num*num;
+
+    // Displaying the multiplication result
+    System.out.println("Output: "+product);
+}
 }
